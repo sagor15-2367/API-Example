9c641a6fec27bbc76c8d952ec46180667159d047
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-12-10 02:34:20 +0600

    api

--- Js/meal.js
+++ Js/meal.js
@@ -1,5 +1,5 @@
 const loadMeals = (searchText) => {
-    const url=`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${ searchText }`
     console.log(url)
     fetch(url)
         .then(res => res.json())
@@ -9,20 +9,24 @@ const loadMeals = (searchText) => {
 const displayMeals = meals => {
     // console.log(meals);
     // step :1 container element
-    const mealsContainer=document.getElementById('meals-container');
-    mealsContainer.innerText='';
+    const mealsContainer = document.getElementById('meals-container');
+    mealsContainer.innerText = '';
     meals.forEach(meal => {
         console.log(meal)
         // step:2 create child for each element
-        const mealDiv=document.createElement('div');
+        const mealDiv = document.createElement('div');
         mealDiv.classList.add('col')
         // step:3 set contain of the  child
-        mealDiv.innerHTML=`
+        mealDiv.innerHTML = `
         <div class="card">
-            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <img src="${ meal.strMealThumb }" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">${meal.strMeal}</h5>
+                <h5 class="card-title">${ meal.strMeal }</h5>
                   <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <!-- Button trigger modal -->
+                  <button onclick="loadMealDetail(${ meal.idMeal })" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+                  Details
+                  </button>
             </div>
         </div>
         `
@@ -31,13 +35,27 @@ const displayMeals = meals => {
     });
 }
 
-const searchMeals=()=>{
-    const searchText=document.getElementById('search-field').value;
+const searchMeals = () => {
+    const searchText = document.getElementById('search-field').value;
     // search meals
     console.log(searchText);
     loadMeals(searchText);
+}
 
+const loadMealDetail = idMeal => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${ idMeal }`
+    console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]))
+}
 
+const displayMealDetails = meal => {
+    document.getElementById('mealDetailsLabel').innerText = meal.strMeal;
+    const mealsDetails = document.getElementById('mealDetailsBody');
+    mealsDetails.innerHTML = `
+<img class="img-fluid" src="${ meal.strMealThumb }">
+`
 }
 
 loadMeals('rice');
